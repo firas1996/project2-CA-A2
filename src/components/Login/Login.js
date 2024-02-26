d7d05803dca1b4711741489759c81b3a035c6d50
firas hamila <firas.hm.1996@gmail.com>
2024-02-26 19:41:54 +0100

    sds

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,48 +1,63 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
+const emailReducer = (state, action) => {
+  if (action.type == "USER_KTEB_EMAIL") {
+    return { value: action.payload, isValid: action.payload.includes("@") };
+  }
+  if (action.type == "USER_NZEL_EL_BARRA") {
+    return { value: state.value, isValid: action.payload.includes("@") };
+  }
+  return { value: "", isValid: null };
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+  const [email, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
   // """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-  useEffect(() => {
-    const t = setTimeout(() => {
-      setFormIsValid(
-        enteredEmail.includes("@") && enteredPassword.trim().length > 6
-      );
-      console.log("Effect");
-    }, 500);
-    return () => {
-      clearTimeout(t);
-      console.log("clean Up");
-    };
-  }, [enteredEmail, enteredPassword]);
+  // useEffect(() => {
+  //   const t = setTimeout(() => {
+  //     setFormIsValid(
+  //       enteredEmail.includes("@") && enteredPassword.trim().length > 6
+  //     );
+  //     console.log("Effect");
+  //   }, 500);
+  //   return () => {
+  //     clearTimeout(t);
+  //     console.log("clean Up");
+  //   };
+  // }, [enteredEmail, enteredPassword]);
 
   // """"""""""""""""""""""""""""""""""""""""""""""
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
-
-    // setFormIsValid(
-    //   event.target.value.includes("@") && enteredPassword.trim().length > 6
-    // );
+    // setEnteredEmail(event.target.value);
+    dispatchEmail({ type: "USER_KTEB_EMAIL", payload: event.target.value });
+    setFormIsValid(
+      event.target.value.includes("@") && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
-    // setFormIsValid(
-    //   event.target.value.trim().length > 6 && enteredEmail.includes("@")
-    // );
+    setFormIsValid(
+      event.target.value.trim().length > 6 && email.value.includes("@")
+    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"));
+    // setEmailIsValid(enteredEmail.includes("@"));
+    dispatchEmail({ type: "USER_NZEL_EL_BARRA" });
   };
 
   const validatePasswordHandler = () => {
@@ -51,7 +66,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(email.value, enteredPassword);
   };
 
   return (
@@ -59,14 +74,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ""
+            email.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={email.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
